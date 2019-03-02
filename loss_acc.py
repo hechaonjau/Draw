c858bf985b7b39e57b2c3fc312b2e6dad85cd1ef
HC <45563110+hechaonjau@users.noreply.github.com>
2019-03-02 16:15:55 +0800

    Update loss_acc.py

--- loss_acc.py
+++ loss_acc.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
 
-def draw(file, x, title):
+def draw(file,a, x, title):
     """
     file:需要提取的文件
+    a:每a步打印一次参数
     x:x轴标题
     title：总标题
     """
@@ -17,7 +18,7 @@ def draw(file, x, title):
     with open(file) as txt:  # 打开文件
         for line in txt:
             line = line.strip()  # 去除多余空格
-            if n % 432 == 0:  # 每一轮打印一次（每轮迭代次数为432）
+            if n % a == 0:  # 每a步打印一次参数
                 line1 = line.split('acc:', 1)  # 在“acc：”处切割字符串分成2个
                 a = (float(line1[1].split(',')[0])) * 0.01  # 再次在“，”处切割字符串，并选取第一个字符串，再将字符串转为浮点数
                 acc_y.append(round(a, 4))  # 保留4位小数
@@ -42,4 +43,4 @@ def draw(file, x, title):
     plt.show()
 
 
-draw('1.txt', 'epoch', 'loss-acc')
+draw('1.txt',432, 'epoch', 'loss-acc')
